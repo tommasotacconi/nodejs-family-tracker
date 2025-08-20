13a7486bb66417368eaa45e0ebc6ee295785d4b0
tommasoTacconi <tommaso.tacconi@gmail.com>
2025-08-21 01:45:12 +0200

    refactor: Simplify visited countries logic by using only visited_countries, implement passIndexData() for repeated index.ejs rendering data

--- index.js
+++ index.js
@@ -21,9 +21,11 @@ let currentUserId = null;
 
 let users = [];
 
-async function checkVisisted(id) {
-  const result = await db.query("SELECT full_name, color, c.country_name, c.country_code FROM users LEFT JOIN user_country ON users.id = user_id LEFT JOIN visited_countries vc ON user_country.country_id = vc.id LEFT JOIN countries c ON vc.country_code = c.country_code WHERE users.id = COALESCE($1, (SELECT id FROM users ORDER BY id LIMIT 1));", [id]);
-	console.log('Fuction "checkVisited() result: ', result.rows);
+async function checkVisited(id) {
+  const result = await db.query("SELECT users.id, full_name, color, c.country_name, c.country_code FROM users LEFT JOIN visited_countries vc ON user_id = users.id LEFT JOIN countries c ON vc.country_id = c.id WHERE users.id = COALESCE($1, (SELECT id FROM users ORDER BY id LIMIT 1));", [id]);
+
+	if (!currentUserId) currentUserId = result.rows[0].id;
+	console.log('Function \'checkVisited()\' result: ', result.rows);
   let countries = [];
   result.rows.forEach(country => {
     if (country.country_code) countries.push(country.country_code);
@@ -32,86 +34,57 @@ async function checkVisisted(id) {
   return [countries, color];
 }
 
+function passIndexData([userCountries, userColor], error = null) {
+	return {
+		countries: userCountries,
+		total: userCountries.length,
+		users,
+		color: userColor,
+		error
+	};
+}
+
 app.get("/", async (req, res) => {
-  const countriesAndColor = await checkVisisted(currentUserId);
+  const userCountriesAndColor = await checkVisited(currentUserId);
 	const { rows: usersData } = await db.query('SELECT id, full_name as name, color FROM users');
 	users = usersData;
-  res.render("index.ejs", {
-    countries: countriesAndColor[0],
-    total: countriesAndColor[0].length,
-    users,
-    color: countriesAndColor[1],
-  });
+  res.render("index.ejs", passIndexData(userCountriesAndColor));
 });
 
 app.post("/add", async (req, res) => {
   const input = req.body["country"];
 
   try {
-		// Retrieve 'country_code' from table 'countries'
-    const result = await db.query(
+		// Retrieve 'id' from 'countries'
+    const { rows: [{ id: countryId, country_code: countryCode }] } = await db.query(
       "SELECT id, country_code FROM countries WHERE LOWER(country_name) LIKE '%' || $1 || '%';",
       [input.toLowerCase()]
     );
+		console.log(`Requested to add country_code ${countryCode} with id ${countryId}`);
 
-    const data = result.rows[0];
-		console.log(data);
-    const countryCode = data.country_code;
-    try {
-			// Retrieve 'country_id' from table 'visited_countries'
-      const { rows: [{ id: countryId }] } = await db.query(
-        "SELECT id FROM visited_countries WHERE country_code = $1;",
-        [countryCode]
-      );
-			console.log("Found country_id in table visited_countries related to country_code: '" + countryCode + "'");
-			try {
-				// Insert user country record in bridge table 'user_country' between 'users' and 'visited_countries'
-				await db.query(
-					"INSERT INTO user_country (user_id, country_id) VALUES ($1, $2)",
-					[currentUserId, countryId]
-				)
-				console.log('Inserted user_id and country_id in user_country table')
-				res.redirect("/");
-			} catch (err) {
-				console.log(err);
-
-				const countriesAndColor = await checkVisisted(currentUserId);
-				res.render("index.ejs", {
-					countries: countriesAndColor[0],
-					total: countriesAndColor[0].length,
-					users: users,
-					color: countriesAndColor[1],
-					error: 'Country already present'
-				});
-			}
-    } catch (err) {
-      console.log(err);
-			const { rows: [{ id: countryId }] } = await db.query("INSERT INTO visited_countries (country_code) VALUES ($1) RETURNING id;", [countryCode]);
+		try {
+			// Insert user country record in bridge table 'visited_countries' between 'users' and 'countries'
 			await db.query(
-				"INSERT INTO user_country (user_id, country_id) VALUES ($1, $2)",
+				"INSERT INTO visited_countries (user_id, country_id) VALUES ($1, $2)",
 				[currentUserId, countryId]
 			)
-			console.log('Not found country_code in table visited_countries. Inserted country_code first, retrieved id and then insert new user-country relation in user_country')
- 
-			const countriesAndColor = await checkVisisted(currentUserId);
-			res.render('index.ejs', {
-				countries: countriesAndColor[0],
-				total: countriesAndColor[0].length,
-				users: users,
-				color: countriesAndColor[1],
-			})
-    }
+			console.log('Inserted user_id and country_id in visited_countries table')
+			res.redirect("/");
+		} catch (err) {
+			/* Failed insertion in bridge table visited_countries */
+			console.log(err);
+			// Warn user the country is already present
+			const userCountriesAndColor = await checkVisited(currentUserId);
+			const errMsg = 'Country already present';
+			res.render("index.ejs", passIndexData(userCountriesAndColor, errMsg));
+		}
   } catch (err) {
+		/* Failed country_code research */
     console.log(err);
-
-		const countriesAndColor = await checkVisisted(currentUserId);
-		res.render('index.ejs', {
-			countries: countriesAndColor[0],
-			total: countriesAndColor[0].length,
-			users: users,
-			color: countriesAndColor[1],
-			error: 'Cannot find a matching world countries, please check the spelling for your country'
-		})
+		// Warn user no matching country is found
+		const userCountriesAndColor = await checkVisited(currentUserId);
+		const errMsg = 'Cannot find a matching world country, please check the spelling for your country'
+		res.render('index.ejs', passIndexData(userCountriesAndColor, errMsg));
 	}
 });
 
